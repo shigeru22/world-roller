16cfe29aa8d384238a696d7416325b6f780d30b0
shigeru22 <61494789+shigeru22@users.noreply.github.com>
2021-04-04 21:07:53 +0700

    Fix tilting by using Slerp

--- Assets/Scripts/Gameplay/Tilt.cs
+++ Assets/Scripts/Gameplay/Tilt.cs
@@ -4,48 +4,13 @@ using UnityEngine;
 
 public class Tilt : MonoBehaviour
 {
-    [SerializeField] Transform worldObject;
-    [SerializeField] Transform ballObject;
     [SerializeField] float tiltSpeed;
-
-    void Start()
-    {
-        GameObject temp = new GameObject();
-        temp.gameObject.name = "Tilter";
-
-        if (worldObject == null)
-        {
-            // assume this object
-            worldObject = GetComponent<Transform>();
-        }
-
-        if(ballObject == null)
-        {
-            // find "ball" or "sphere" later
-        }
-
-        gameObject.transform.parent = temp.transform;
-        ballObject.transform.parent = temp.transform;
-
-        Vector3 angle = temp.transform.localEulerAngles;
-        angle.x -= 10f;
-        angle.z -= 10f;
-        temp.transform.localEulerAngles = angle;
-
-        // Debug.Log($"Target object rotation: {worldObject.localEulerAngles.x}, {worldObject.localEulerAngles.y}, {worldObject.localEulerAngles.z}");
-    }
 
     void Update()
     {
-        Vector3 temp = worldObject.localEulerAngles;
-
-        temp.x = Mathf.Lerp(temp.x, InputManager.Instance.vertical * 10, 0.5f);
-        temp.z = Mathf.Lerp(temp.z, -InputManager.Instance.horizontal * 10, 0.5f);
-        temp.x += 5f;
-        temp.z += 5f;
-
-        worldObject.localEulerAngles = temp;
-
-        // Debug.Log(temp);
+        float horizontal = -InputManager.Instance.horizontal * 10f;
+        float vertical = InputManager.Instance.vertical * 10f;
+        var target = Quaternion.Euler(vertical, 0, horizontal);
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * tiltSpeed);
     }
 }
