bfd83dc7ed5dd9dae750ef50b3b9481097c0d675
shigeru22 <61494789+shigeru22@users.noreply.github.com>
2021-05-09 12:41:50 +0700

    Follow tilt input based on rotation

--- Assets/Scripts/Gameplay/Tilt.cs
+++ Assets/Scripts/Gameplay/Tilt.cs
@@ -8,9 +8,33 @@ public class Tilt : MonoBehaviour
 
     void Update()
     {
-        float horizontal = -InputManager.Instance.horizontal * 20f;
-        float vertical = InputManager.Instance.vertical * 20f;
-        var target = Quaternion.Euler(vertical, 0, horizontal);
+        float horizontal, vertical;
+        int rotation = (int)Mathf.Abs(GameManager.Instance.worldRotation / 90f % 4f);
+
+        if(rotation == 1)
+        {
+            Debug.Log("enter");
+            horizontal = InputManager.Instance.vertical * 20f;
+            vertical = InputManager.Instance.horizontal * 20f;
+        }
+        else if(rotation == 2)
+        {
+            horizontal = InputManager.Instance.horizontal * 20f;
+            vertical = -InputManager.Instance.vertical * 20f;
+        }
+        else if(rotation == 3)
+        {
+            // 90
+            horizontal = -InputManager.Instance.vertical * 20f;
+            vertical = -InputManager.Instance.horizontal * 20f;
+        }
+        else
+        {
+            horizontal = -InputManager.Instance.horizontal * 20f;
+            vertical = InputManager.Instance.vertical * 20f;
+        }
+
+        Quaternion target = Quaternion.Euler(vertical, 0, horizontal);
         transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * tiltSpeed);
     }
 }
